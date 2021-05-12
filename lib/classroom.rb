2bf15987c47c70d82855c624098cfa9e8e975aff
Carina Sweet <carina.sweet06@gmail.com>
2021-05-12 15:59:10 -0600

    Add Classroom code and additional tests

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,24 @@
 class Classroom
+  attr_reader :subject,
+              :capacity,
+              :students
+
+  def initialize(subject, capacity)
+    @subject = subject
+    @capacity = capacity
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def yell_at_students
+    @students.each.upcase
+  end
+
+  def over_capacity
+    @students.count > @capacity
+  end
+
 end
